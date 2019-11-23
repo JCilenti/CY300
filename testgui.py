8ec1dcad5a08d934b381fe91f7872929465da38e
JCilenti <43307460+JCilenti@users.noreply.github.com>
2019-11-23 00:08:23 -0500

    Updated GUI

--- testgui.py
+++ testgui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk,Image
 from tkinter import filedialog
 
 root = Tk()
@@ -6,9 +7,14 @@ root.title("Welcome to Our Colorization Tool")
 root.geometry("500x500")
 root.configure(background='light blue')
 
+global img
 def browsefunc():
     filename = filedialog.askopenfilename()
     pathlabel.config(text=filename)
+    canvas = Canvas(root, width = 300, height = 300)
+    canvas.pack()
+    img = ImageTk.PhotoImage(file = filename)
+    canvas.create_image(20, 20, anchor=NW, image = img)
 
 browsebutton = Button(root, text="Browse", command=browsefunc)
 browsebutton.pack()
