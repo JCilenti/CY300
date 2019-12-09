71a17d8689fab7c8955f289451dc99d3b329181f
JCilenti <43307460+JCilenti@users.noreply.github.com>
2019-12-09 14:36:30 -0500

    Updates

--- testgui.py
+++ testgui.py
@@ -2,29 +2,31 @@ from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import filedialog
 
-root = Tk()
-root.title("Welcome to Our Colorization Tool")
-root.geometry("500x500")
-root.configure(background='light blue')
+window = Tk()
+window.configure(background='light blue')
+window.title("Welcome to Our Colorization Tool")
+window.geometry("500x500")
+
 
 
 def browsefunc():
     filename = filedialog.askopenfilename()
     pathlabel.config(text=filename)
-    canvas = Canvas(root)
+    canvas = Canvas(window)
     img = ImageTk.PhotoImage(file = filename)
+    print(img)
     canvas.create_image(20, 20, anchor=NW, image = img)
     canvas.pack()
     # look for a .draw function
 
-browsebutton = Button(root, text="Browse", command=browsefunc)
+browsebutton = Button(window, text="Browse", command=browsefunc)
 browsebutton.pack()
 
-startbutton = Button(root, text="Start Process")
+startbutton = Button(window, text="Start Process")
 startbutton.pack()
 
-pathlabel = Label(root)
+pathlabel = Label(window)
 pathlabel.pack()
 
 #mainloop()
-root.mainloop()
+mainloop()
